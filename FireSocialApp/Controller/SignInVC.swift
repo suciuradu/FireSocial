f60c57e61b530bdf456199c5dc12338076929a12
suciuradu <suciu197@yahoo.com>
2017-10-11 00:18:01 +0300

    modified and powerline installed

---
+++ FireSocialApp/Controller/SignInVC.swift
@@ -0,0 +1,26 @@
+//
+//  SignInVC.swift
+//  FireSocialApp
+//
+//  Created by Suciu Radu on 10/10/2017.
+//  Copyright © 2017 Suciu Radu. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-FireSocialApp/Pods-FireSocialApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FireSocialApp : NSObject
+@end
+@implementation PodsDummy_Pods_FireSocialApp
+@end
