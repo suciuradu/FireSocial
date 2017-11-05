c59b6a00c577775e8601ea269a2d95ec927430d5
suciuradu <suciu197@yahoo.com>
2017-11-06 01:19:42 +0200

    Facebook Authentifaction Works

--- FireSocialApp/Controller/SignInVC.swift
+++ FireSocialApp/Controller/SignInVC.swift
@@ -41,7 +41,7 @@ class SignInVC: UIViewController {
         // Custom Duration
         pastelView.animationDuration = 3.0
         // Custom Color
-        pastelView.setColors( [#colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1),#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1),#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)])
+        pastelView.setColors( [#colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1),#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1),#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1),#colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)])
         pastelView.startAnimation()
     }
     
