2d2501557a535d33cfc34f55697b26660170a875
suciuradu <suciu197@yahoo.com>
2017-11-05 12:53:28 +0200

    Added the Pastel View

--- FireSocialApp/Controller/SignInVC.swift
+++ FireSocialApp/Controller/SignInVC.swift
@@ -7,13 +7,28 @@
 //
 
 import UIKit
+import Pastel
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var pastelView: PastelView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("now works")
+        
+        pastelView.startPastelPoint = .bottomLeft
+        pastelView.endPastelPoint = .topRight
+
+
+        // Custom Duration
+        pastelView.animationDuration = 10.0
+
+        // Custom Color
+        pastelView.setColors([#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1), #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1),#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1) ])
+
+        pastelView.startAnimation()
     }
 
     override func didReceiveMemoryWarning() {
