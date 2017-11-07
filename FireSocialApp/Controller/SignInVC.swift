5890912424d395093584b888c1376b4d202627d9
suciuradu <suciu197@yahoo.com>
2017-11-07 15:04:16 +0200

    Added Email Authentification - Works

--- FireSocialApp/Controller/SignInVC.swift
+++ FireSocialApp/Controller/SignInVC.swift
@@ -13,12 +13,14 @@ import FacebookLogin
 import Firebase
 import FBSDKLoginKit
 import FBSDKCoreKit
+import SkyFloatingLabelTextField
 
 class SignInVC: UIViewController {
 
     @IBOutlet weak var pastelView: PastelView!
     
-   
+    @IBOutlet weak var emailField: SkyFloatingLabelTextField!
+    @IBOutlet weak var passwordField: SkyFloatingLabelTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,11 +41,30 @@ class SignInVC: UIViewController {
         pastelView.startPastelPoint = .bottomLeft
         pastelView.endPastelPoint = .topRight
         // Custom Duration
-        pastelView.animationDuration = 3.0
+        pastelView.animationDuration = 5.0
         // Custom Color
         pastelView.setColors( [#colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1),#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1),#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1),#colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)])
         pastelView.startAnimation()
     }
+    
+    @IBAction func signInBtnPressed(_ sender: Any) {
+        if let email = emailField.text, let pwd = passwordField.text {
+            Auth.auth().signIn(withEmail: email, password: pwd, completion: {(user, error) in
+                if error == nil {
+                    print("Suciu: Email signed in with Firebase")
+                } else {
+                    Auth.auth().createUser(withEmail: email, password: pwd, completion: {(user, error) in
+                        if error != nil {
+                            print("Suciu: Unable to auth to firebase with email")
+                        } else {
+                            print("Suciu: Succes to firebase with email")
+                        }
+                    })
+                }
+            })
+        }
+    }
+    
     
     @IBAction func facebookBtnPressed(_ sender: Any) {
         
@@ -55,7 +76,7 @@ class SignInVC: UIViewController {
             } else if result?.isCancelled == true {
                 print ("Suciu : user canceled authent")
             } else {
-                print("Suciu: Succes!")
+                print("Suciu: Facebook auth Succes!")
                 let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
                 self.firebaseAuth(credential)
             }
