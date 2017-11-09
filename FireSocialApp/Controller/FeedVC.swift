57c2b64cef4c3a2d451e9b20e7be6b191ff78b81
suciuradu <suciu197@yahoo.com>
2017-11-09 15:57:29 +0200

    Added FeedVC and some ui

--- FireSocialApp/Controller/FeedVC.swift
+++ FireSocialApp/Controller/FeedVC.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class FeedVC: UIViewController {
+    
+    @IBOutlet weak var centerYPopUpConstraint: NSLayoutConstraint!
 
+    @IBOutlet weak var postView: RoundedView!
+    @IBOutlet weak var blurView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,4 +22,32 @@ class FeedVC: UIViewController {
     }
 
   
+    @IBAction func postBtnPressed(_ sender: Any) {
+        
+        //nu uita sa setezi Y constrint de la postView pe 630
+        self.centerYPopUpConstraint.constant = 630
+        self.view.layoutIfNeeded()
+        
+        UIView.animate(withDuration: 0.45, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseIn, animations: {
+            
+            self.blurView.alpha = 0.55
+            self.centerYPopUpConstraint.constant = 0
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        
+        //nu uita sa setezi Y constrint de la postView pe 630
+        self.centerYPopUpConstraint.constant = 0
+        self.view.layoutIfNeeded()
+        
+        UIView.animate(withDuration: 0.45, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseIn, animations: {
+            
+            self.blurView.alpha = 0
+            self.centerYPopUpConstraint.constant = 630
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
 }
---
+++ FireSocialApp/View/RoundedView.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedView.swift
+//  FireSocialApp
+//
+//  Created by Suciu Radu on 07/11/2017.
+//  Copyright © 2017 Suciu Radu. All rights reserved.
+//
+
+import UIKit
+
+class RoundedView: UIView {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.cornerRadius = 15.0
+    }
+
+}
