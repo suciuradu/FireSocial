92a27340fd742efc9bd6982c1b1655503853838f
suciuradu <suciu197@yahoo.com>
2017-11-17 02:19:39 +0200

    Firebase

--- FireSocialApp/Controller/FeedVC.swift
+++ FireSocialApp/Controller/FeedVC.swift
@@ -24,7 +24,9 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.delegate = self
         tableView.dataSource = self
         
-
+        DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
+            print(snapshot.value as Any)
+        })
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
